f6b9eeaf91b44c6baab105af950221f16ddc4387
Adrian Grebin <agrebin@vurbia.com>
2018-03-02 11:52:30 -0300

    First working revision

--- runplay.sh
+++ runplay.sh
@@ -1,1 +1,1 @@
-ansible-playbook -vvvvvvvv -i inventory ecs.yml   -e my_route53_zone=prototype.local  -e ssh_key_name=Adrian
+ansible-playbook -vvvvvvvv -i inventory deploy.yml   -e my_route53_zone=prototype.local  -e ssh_key_name=Adrian 
